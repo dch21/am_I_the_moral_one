d5a619fcdc2b38e6ef2c31018b535f397985970d
dchu <danchu56@gmail.com>
2020-10-05 09:31:02 -0400

    Git ignore

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://mongoUser:fDci3yRWYWKAbeeI@cluster0.of77i.mongodb.net/IAmTheMoralOne?retryWrites=true&w=majority',
-    secretOrKey: 'fDci3yRWYWKAbeeI'
-}
-
