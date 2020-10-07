7aeda478de18989b596615db7d46a0cb9a85a5e2
fjswayze <fjswayze@vassar.edu>
2020-10-06 19:13:39 -0700

    changed app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -13,13 +13,6 @@ const express = require("express");
 const app = express();
 
 
-const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
 
 
 
