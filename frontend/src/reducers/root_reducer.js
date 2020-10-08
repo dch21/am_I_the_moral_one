9d2e9d3d6c96501c385f310cb9773aee3890306e
fjswayze <fjswayze@vassar.edu>
2020-10-08 14:07:03 -0700

    resolved conflict

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer'; 
 import questions from './questions_reducer'; 
-import quizzes from './quiz_reducer';
 import prompts from  "./prompts_reducer";
 
 const RootReducer = combineReducers({
     session, 
     errors, 
     questions,
-    quizzes,
     prompts
 });
 
