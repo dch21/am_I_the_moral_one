1f535c9bef82bef3c9d97d7b706970a9528699ea
azessar <akzessar@gmail.com>
2020-10-07 22:17:37 -0500

    graphs on page

--- frontend/src/components/homepage/pie_chart.js
+++ frontend/src/components/homepage/pie_chart.js
@@ -8,39 +8,84 @@ import {
 export default class MainPieChart extends PureComponent {
     
     componentDidMount() {
-        // debugger
-        this.props.fetchQuestionData(2);
-        // debugger
+        this.props.fetchQuestionData(this.props.questionNumber);
     }
 
-    
+    // componentWillUpdate(){
+    //     this.props.fetchQuestionData(this.props.questionNumber)
+    // }
 
-    
+
 
     render() {
-        // debugger
 
         if (!this.props.questionData.data) {
             return null;
         }
-        const genderData = this.props.questionData.data.question.questionChoices.choiceA.demo.gender;
+        // debugger
+        const questionText = this.props.questionData.data.question.text
+        const choiceAtext = this.props.questionData.data.question.choiceAText
+        const choiceBtext = this.props.questionData.data.question.choiceBText
+        const labels = Object.keys(this.props.questionData.data.question.questionChoices.choiceA.demo.gender)
+
+        const genderDataA = this.props.questionData.data.question.questionChoices.choiceA.demo.gender;
         
-        const male = genderData["Male"];
-        const female = genderData["Female"];
-        const nonbinary = genderData["Non-Binary"];
-        const other = genderData["Other"];
-        const preferNotToID = genderData["Prefer-Not-To-Self-Id"]
+        const maleA = genderDataA["Male"];
+        const femaleA = genderDataA["Female"];
+        const nonbinaryA = genderDataA["Non-Binary"];
+        const otherA = genderDataA["Other"];
+        const preferNotToIDA = genderDataA["Prefer-Not-To-Self-Id"]
+
+        const genderDataB = this.props.questionData.data.question.questionChoices.choiceB.demo.gender;
+        
+        const maleB = genderDataB["Male"];
+        const femaleB = genderDataB["Female"];
+        const nonbinaryB = genderDataB["Non-Binary"];
+        const otherB = genderDataB["Other"];
+        const preferNotToIDB = genderDataB["Prefer-Not-To-Self-Id"]
 
 
         const data = [
-            { name: 'Group A', value: male },
-            { name: 'Group B', value: female },
-            { name: 'Group C', value: nonbinary },
-            { name: 'Group D', value: other },
-            { name: 'Group E', value: preferNotToID }
+            { name: 'Group A', value: maleA },
+            { name: 'Group B', value: maleB },
+            // { name: 'Group C', value: nonbinaryA },
+            // { name: 'Group D', value: otherA },
+            // { name: 'Group E', value: preferNotToIDA }
         ];
 
-        const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+        const dataPrefer = [
+            { name: 'Group A', value: preferNotToIDA },
+            { name: 'Group B', value: preferNotToIDB },
+            // { name: 'Group C', value: nonbinaryA },
+            // { name: 'Group D', value: otherA },
+            // { name: 'Group E', value: preferNotToIDA }
+        ];
+
+        const dataFemale = [
+            { name: 'Group A', value: femaleA },
+            { name: 'Group B', value: femaleB },
+            // { name: 'Group C', value: nonbinaryA },
+            // { name: 'Group D', value: otherA },
+            // { name: 'Group E', value: preferNotToIDA }
+        ];
+        
+        const dataNonBinary = [
+            { name: 'Group A', value: nonbinaryA },
+            { name: 'Group B', value: nonbinaryA },
+            // { name: 'Group C', value: nonbinaryA },
+            // { name: 'Group D', value: otherA },
+            // { name: 'Group E', value: preferNotToIDA }
+        ];
+
+        const dataOther = [
+            { name: 'Group A', value: otherA },
+            { name: 'Group B', value: otherB },
+            // { name: 'Group C', value: nonbinaryA },
+            // { name: 'Group D', value: otherA },
+            // { name: 'Group E', value: preferNotToIDA }
+        ];
+
+        const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#bada55',  '#92A0F2'];
 
         const RADIAN = Math.PI / 180;
         const renderCustomizedLabel = ({
@@ -58,22 +103,99 @@ export default class MainPieChart extends PureComponent {
         };
 
         return (
-            <PieChart width={400} height={400}>
-                <Pie
-                    data={data}
-                    cx={200}
-                    cy={200}
-                    labelLine={false}
-                    label={renderCustomizedLabel}
-                    outerRadius={80}
-                    fill="#8884d8"
-                    dataKey="value"
-                >
-                    {
-                        data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-                    }
-                </Pie>
-            </PieChart>
+            <div className="data-charts">
+                <p>{questionText}</p>
+                <p>Blue:{choiceAtext}</p>
+                <p>Green:{choiceBtext}</p>
+
+
+                    <p>{labels[0]}</p>
+                <PieChart width={400} height={400}>
+                    <Pie 
+                        data={data}
+                        cx={200}
+                        cy={200}
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {
+                            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                        }
+                    </Pie>
+                </PieChart>
+
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={dataFemale}
+                        cx={200}
+                        cy={200}
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {
+                            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                        }
+                    </Pie>
+                </PieChart>
+
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={dataPrefer}
+                        cx={200}
+                        cy={200}
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {
+                            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                        }
+                    </Pie>
+                </PieChart>
+
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={dataNonBinary}
+                        cx={200}
+                        cy={200}
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {
+                            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                        }
+                    </Pie>
+                </PieChart>
+
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={dataOther}
+                        cx={200}
+                        cy={200}
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {
+                            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                        }
+                    </Pie>
+                </PieChart>
+            </div>
+
         );
     }
 }
---
+++ frontend/src/components/results/results.js
@@ -0,0 +1,59 @@
+import React from "react";
+import '../homepage/homepage.scss'
+import MainPieChart from '../homepage/piechart_container'
+// import DataVis from './data_vis'; 
+
+class ResultsPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            questionNumber: 1
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(field) {
+        
+        // field.preventDefault()
+        this.setState({
+            questionNumber: field
+        })
+        console.log(this.state)
+        // debugger
+    }
+
+    render() {
+        // debugger
+        return (
+            <div className="splash-div">
+                <div className="quizzes-taken-directory">
+                    <h1 className="quizzes-taken-header">Questions</h1>
+                    <div className="quizzes-taken-links">
+                        <div onClick={() => this.handleClick(1)}>1</div>
+                        <div onClick={() => this.handleClick(2)}>2</div>
+                        <div onClick={() => this.handleClick(3)}>3</div>
+                        <div onClick={() => this.handleClick(4)}>4</div>
+                        <div onClick={() => this.handleClick(5)}>5</div>
+                        <div onClick={() => this.handleClick(6)}>6</div>
+                        <div onClick={() => this.handleClick(7)}>7</div>
+                        <div onClick={() => this.handleClick(8)}>8</div>
+                        <div onClick={() => this.handleClick(9)}>9</div>
+                        <div onClick={() => this.handleClick(10)}>10</div>
+                    </div>
+                </div>
+               
+                <div className="main-data-div">
+                    <h1 className="data-header">Data:</h1>
+                    <div className="inner-data-box">
+                        <MainPieChart questionNumber={this.state.questionNumber} />
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default ResultsPage;
