1988ade31b2a5d3a89da584072259462b857413b
azessar <akzessar@gmail.com>
2020-10-22 17:04:18 -0500

    adding parts of user info

--- frontend/src/components/user_info/user_info.js
+++ frontend/src/components/user_info/user_info.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import './user_info.css'
 
 class UserInfo extends React.Component {
     constructor(props) {
@@ -13,11 +14,36 @@ class UserInfo extends React.Component {
 
 
     render() {
-
+        let user = this.props.currentUser
         return (
 
             <div>
-                TEST TEST TEST
+                <div className="user-info-columns">
+                    <div className="user-info-column">
+                        <h1>User Info</h1>
+                        <div className="inside-user-column">
+                            <div>Email: {user.email}</div>
+                            <div>Education: {user.education}</div>
+                            <div>Ethnicity: {user.ethnicity}</div>
+                            <div>Location: {user.location}</div>
+                            <div>Pet Choice: {user.petChoice}</div>
+                            <div>Political Leaning: {user.politicalLeaning}</div>
+                            <div>Religious Affiliation: {user.religiousAffiliation}</div>
+                        </div>
+                    </div>
+                    <div className="user-info-column">
+                        <h1>Quizzes Taken</h1>
+                        <div className="inside-user-column">
+                            something
+                        </div>
+                    </div>
+                    <div className="user-info-column">
+                        <h1>Quizzes Submitted</h1>
+                        <div className="inside-user-column">
+                            something
+                        </div>
+                    </div>
+                </div>
             </div>
 
         )
--- frontend/src/components/user_info/user_info_container.js
+++ frontend/src/components/user_info/user_info_container.js
@@ -4,13 +4,15 @@ import UserInfo from './user_info';
 
 const mapStateToProps = (state) => {
     return {
-        errors: state.errors.session
+        errors: state.errors.session,
+        currentUser: state.session.user
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        login: user => dispatch(login(user))
+        login: user => dispatch(login(user)),
+
     }
 }
 
