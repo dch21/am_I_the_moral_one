aaa8f342255ed5e6bf1b1e12596010963d5d47d1
fjswayze <fjswayze@vassar.edu>
2020-10-26 08:05:16 -0700

    close to final push

--- frontend/src/components/quizzes/create_quiz_container.js
+++ frontend/src/components/quizzes/create_quiz_container.js
@@ -10,17 +10,7 @@ const mSTP = (state) => ({
     //totalQuizzes is amount of 10 question Quizzes
     totalQuizzes: state.totals,
     user: state.session.user,
-    // question1: state.questions[1] || {},
-    // question2: state.questions[2] || {},
-    // question3: state.questions[3] || {},
-    // question4: state.questions[4] || {},
-    // question5: state.questions[5] || {},
-    // question6: state.questions[6] || {},
-    // question7: state.questions[7] || {},
-    // question8: state.questions[8] || {},
-    // question9: state.questions[9] || {},
-    // question10: state.questions[10] || {}
-
+  
 })
 
 const mDTP = dispatch => {
